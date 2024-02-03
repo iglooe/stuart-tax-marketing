8c29f47a379e06ea37b2e66413ef86f7dc6cddad
iglooe <day.derek7@protonmail.com>
2024-02-02 21:06:25 -0600

    style(mobile nav) - give same styles from desktop nav to mobile

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -5,6 +5,8 @@ import { HamburgerMenuIcon } from "@radix-ui/react-icons"
 import { useTranslations } from "next-intl"
 
 import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { useScrollTop } from "@/hooks/use-scroll-top"
 
 import { Icons } from "./icons"
 import LocaleSwitcher from "./locale-switcher"
@@ -16,35 +18,48 @@ import { Separator } from "./ui/separator"
 export const MobileNav = () => {
   const t = useTranslations("Navigation")
 
+  const scrolled = useScrollTop()
+
   return (
     <>
-      <Drawer direction="right">
-        <DrawerTrigger>
-          <div className="sticky top-0 z-50 flex h-14 w-full items-center border border-border/50 bg-gray-200 pl-4">
-            <Button variant="ghost" size="icon">
-              <HamburgerMenuIcon className="size-8" />
-            </Button>
-          </div>
-        </DrawerTrigger>
-        <DrawerContent>
-          <div className="bg-background pl-4 pt-4">
-            <Link href="/" className="flex items-center space-x-2">
-              <Icons.logo className="size-6" />
-              <span className="inline-block font-bold">{siteConfig.name}</span>
-            </Link>
-            <Separator className="my-4" />
-            <div className="flex flex-col">
-              <NavigationLink href="/">{t("home")}</NavigationLink>
-              <NavigationLink href="/about">{t("about")}</NavigationLink>
-              <NavigationLink href="/services">{t("services")}</NavigationLink>
-              <NavigationLink href="/contact">{t("contact")}</NavigationLink>
+      <div
+        className={cn(
+          "sticky top-0 z-40 flex h-16 w-full items-center bg-gray-200 px-4 dark:bg-[#090d1a] lg:px-6",
+          scrolled && "border-b shadow-sm"
+        )}
+      >
+        <Drawer direction="right">
+          <DrawerTrigger>
+            <div className="flex h-14 w-full items-center">
+              <Button variant="ghost" size="icon">
+                <HamburgerMenuIcon className="size-8" />
+              </Button>
             </div>
-            <div className="inset-x-0 bottom-0 mt-24 flex justify-center">
-              <LocaleSwitcher />
+          </DrawerTrigger>
+          <DrawerContent>
+            <div className="bg-background pl-4 pt-4">
+              <Link href="/" className="flex items-center space-x-2">
+                <Icons.logo className="size-6" />
+                <span className="inline-block font-bold">
+                  {siteConfig.name}
+                </span>
+              </Link>
+              <Separator className="my-4" />
+              <div className="flex flex-col">
+                <NavigationLink href="/">{t("home")}</NavigationLink>
+                <NavigationLink href="/about">{t("about")}</NavigationLink>
+                <NavigationLink href="/services">
+                  {t("services")}
+                </NavigationLink>
+                <NavigationLink href="/contact">{t("contact")}</NavigationLink>
+              </div>
+              <div className="inset-x-0 bottom-0 mt-24 flex justify-center">
+                <LocaleSwitcher />
+              </div>
             </div>
-          </div>
-        </DrawerContent>
-      </Drawer>
+          </DrawerContent>
+        </Drawer>
+      </div>
     </>
   )
 }
