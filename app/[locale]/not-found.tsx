54a5de1faad661ac548a491c8b4da948b51c4de3
iglooe <day.derek7@protonmail.com>
2024-02-23 00:37:15 -0600

    style(404): restyle not-found page

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -11,22 +11,31 @@ export default function NotFoundPage() {
   const t = useTranslations("NotFoundPage")
 
   return (
-    <>
-      <Shell>
-        <section className="backdrop-blur-[2px]md:py-24 w-full rounded-lg border border-border/50 bg-background/50 p-4 py-16 lg:py-32">
-          <h2 className="flex scroll-m-20 justify-center text-4xl font-extrabold tracking-tight lg:text-5xl">
-            {t("title")}
-          </h2>
-          <p className="flex justify-center leading-7 [&:not(:first-child)]:mt-6">
-            {t("description")}
-          </p>
-          <div className="mt-20 flex items-center justify-center">
-            <Button size="lg" variant="secondary">
-              <Link href="/">{t("action")}</Link>
-            </Button>
-          </div>
-        </section>
-      </Shell>
-    </>
+    <main className="grid min-h-full place-items-center bg-transparent px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-primary">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-black dark:text-white sm:text-5xl">
+          {t("title")}
+        </h1>
+        <p className="mt-6 text-base leading-7 text-muted-foreground">
+          {t("description")}
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Button variant="default">
+            <Link className="text-companyDark" href="/">
+              {t("action")}
+            </Link>
+          </Button>
+          <Button variant="link">
+            <Link
+              href="/about"
+              className="text-sm font-semibold text-companyDark"
+            >
+              {t("secondary-action")} <span aria-hidden="true">&rarr;</span>
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </main>
   )
 }
