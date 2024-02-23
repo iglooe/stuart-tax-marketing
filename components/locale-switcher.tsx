b2b6784f1ded2eaaea488486c7084439071d1e9d
iglooe <day.derek7@protonmail.com>
2024-02-23 16:59:57 -0600

    style(locale-switcher): completely re-do the locale switcher

--- components/locale-switcher.tsx
+++ components/locale-switcher.tsx
@@ -1,19 +1,45 @@
+import { useTransition } from "react"
 import { locales } from "@/i18n/config"
+import { usePathname, useRouter } from "@/i18n/navigations"
 import { useLocale, useTranslations } from "next-intl"
 
-import LocaleSwitcherSelect from "./locale-switcher-select"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select"
 
 export default function LocaleSwitcher() {
-  const t = useTranslations("LocaleSwitcher")
+  const [isPending, startTransition] = useTransition()
+  const router = useRouter()
+  const pathname = usePathname()
   const locale = useLocale()
 
+  function onSelectChange(selectedLocale: string) {
+    const nextLocale = selectedLocale
+    startTransition(() => {
+      router.replace(pathname, { locale: nextLocale })
+    })
+    router.refresh()
+  }
+
+  const t = useTranslations("LocaleSwitcher")
+
   return (
-    <LocaleSwitcherSelect defaultValue={locale} label={t("label")}>
-      {locales.map((cur: string) => (
-        <option key={cur} value={cur}>
-          {t("locale", { locale: cur })}
-        </option>
-      ))}
-    </LocaleSwitcherSelect>
+    <Select onValueChange={onSelectChange} value={locale}>
+      <SelectTrigger className="w-[120px]" disabled={isPending}>
+        <SelectValue placeholder={t("label")} />
+      </SelectTrigger>
+      <SelectContent>
+        {locales.map((cur: string) => (
+          <SelectGroup key={cur}>
+            <SelectItem value={cur}>{t("locale", { locale: cur })}</SelectItem>
+          </SelectGroup>
+        ))}
+      </SelectContent>
+    </Select>
   )
 }
